8ebf0bc7dfb17dd34d643e48d621e8d6631a420a
autimio <autimio.dev@gmail.com>
2021-02-09 21:11:23 -0300

    fix: refactor onChangeInputText

--- src/App.js
+++ src/App.js
@@ -8,37 +8,23 @@ function App() {
   const [greenValue, setGreenValue] = useState(0);
   const [blueValue, setBlueValue] = useState(0);
 
-  function onChangeInputText(color, event) {
-    if (event.target.value === isNaN) return;
-    if (event.target.value > 255) return alert("Opa, numero passou do limite!");
-
-    switch (color) {
-      case "red":
-        setRedValue(event.target.value);
-        break;
-      case "green":
-        setGreenValue(event.target.value);
-        break;
-      case "blue":
-        setBlueValue(event.target.value);
-        break;
-      default:
-        break;
-    }
+  function onChangeInputText(event, setState) {
+    const { value } = event.target;
+    if (value === isNaN) return;
+    if (value > 255) return alert("Opa, numero passou do limite!");
+    setState(value);
   }
 
-  function rgbToHex(rgb) {
-    var hex = Number(rgb).toString(16);
-    if (hex.length < 2) {
-      hex = "0" + hex;
-    }
+  function base10Tobase16(number) {
+    let hex = Number(number).toString(16);
+    if (hex.length < 2) hex = "0" + hex;
     return hex;
   }
 
-  function fullColorHex(r, g, b) {
-    var red = rgbToHex(r);
-    var green = rgbToHex(g);
-    var blue = rgbToHex(b);
+  function colorHex(r, g, b) {
+    const red = base10Tobase16(r);
+    const green = base10Tobase16(g);
+    const blue = base10Tobase16(b);
     return `#${red}${green}${blue}`;
   }
 
@@ -57,7 +43,7 @@ function App() {
           type={"number"}
           value={redValue}
           maxLength={3}
-          onChange={(event) => onChangeInputText("red", event)}
+          onChange={(event) => onChangeInputText(event, setRedValue)}
         />
 
         <InputText
@@ -67,7 +53,7 @@ function App() {
           type={"number"}
           value={greenValue}
           maxLength={3}
-          onChange={(event) => onChangeInputText("green", event)}
+          onChange={(event) => onChangeInputText(event, setGreenValue)}
         />
 
         <InputText
@@ -77,7 +63,7 @@ function App() {
           type={"number"}
           value={blueValue}
           maxLength={3}
-          onChange={(event) => onChangeInputText("blue", event)}
+          onChange={(event) => onChangeInputText(event, setBlueValue)}
         />
       </div>
 
@@ -85,10 +71,9 @@ function App() {
         Hexadecimal Color:
         <label id="text-hexadecimal-color-result">
           {" "}
-          {fullColorHex(redValue, greenValue, blueValue)}{" "}
+          {colorHex(redValue, greenValue, blueValue)}
         </label>
       </label>
-      {/* <FaCopy color="green" /> */}
     </div>
   );
 }
